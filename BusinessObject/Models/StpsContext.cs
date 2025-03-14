48fb6d69340792567d010595fc4e139a423ead30
+Tinh Nguyễn _ <tinhnguyen1582002@gmail.com>
2025-03-14 11:46:25 +0700

    add app

--- BusinessObject/Models/StpsContext.cs
+++ BusinessObject/Models/StpsContext.cs
@@ -39,7 +39,7 @@ public partial class StpsContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=TINHNGUYEN\\MSSQLSERVER01; Database=STPS; User Id=sa1; Password=123; Encrypt=False; MultipleActiveResultSets=True;");
+        => optionsBuilder.UseSqlServer("Server=(local); Database=STPS; User Id=sa1; Password=123; Encrypt=False; MultipleActiveResultSets=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
